941c053571dd288a61921a6bdf8cf93428bf0c0d
Doug Wright <mrdougwright@gmail.com>
2018-02-17 17:43:58 -0800

    add init and call methods

--- lib/todo.ex
+++ lib/todo.ex
@@ -1,18 +1,16 @@
 defmodule Todo do
-  @moduledoc """
-  Documentation for Todo.
-  """
 
-  @doc """
-  Hello world.
+  # server methods
 
-  ## Examples
+  # init/1 is called once when the server is started
+  def init(default_opts) do
+    IO.puts "starting up Todo..."
+    default_opts
+  end
 
-      iex> Todo.hello
-      :world
-
-  """
-  def hello do
-    :world
+  # call/2 is called every time a new request comes in.
+  def call(conn, _opts) do
+    IO.puts "saying hello!"
+    Plug.Conn.send_resp(conn, 200, "Hello, world!")
   end
 end
