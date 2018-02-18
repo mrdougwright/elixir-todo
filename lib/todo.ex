cd36ac4952863076dc9de1b0c1201bcdb4f2bf8a
Doug Wright <mrdougwright@gmail.com>
2018-02-17 18:00:18 -0800

    add view template

--- lib/todo.ex
+++ lib/todo.ex
@@ -19,9 +19,11 @@ defmodule Todo do
     send_response(conn, 200, "Hello, world!")
   end
 
-  def route("GET", ["users", user_id], conn) do
-    # this route is for /users/<user_id>
-    send_response(conn, 200, "You requested user #{user_id}")
+  def route("GET", ["todo", list_item], conn) do
+    page_contents = EEx.eval_file("templates/show_item.eex", [list_item: list_item])
+    conn
+    |> Plug.Conn.put_resp_content_type("text/html")
+    |> send_response(200, page_contents)
   end
 
   def route(_method, _path, conn) do
